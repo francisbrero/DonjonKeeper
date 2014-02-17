951028c38ea20037404dc2acd2388d8b71b6ac85
Francis-a1 <francis.brero@agilone.com>
2014-02-16 16:23:07 -0800

    postgre2

--- hello.js
+++ hello.js
@@ -1,12 +1,15 @@
 // hello.js
 var express = require("express");
 var logfmt = require("logfmt");
+var pg = require('pg');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Wazzup Bitch?! The garage door status is: ');
+  // PostGre calls
+  getStatus(disconnectAll);
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -14,13 +17,19 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-// PostGre calls
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//returns current status
+function getStatus(onDone){
+ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   client.query('SELECT status from donjonkeeper order by dateModified DESC limit 1', function(err, result) {
     done();
-    if(err) return console.error(err);
-    console.log(result.rows);
+    if(err) return console.error(err);    
+	console.log(result.rows);
+	onDone();
+   });
   });
-});
+}
+
+// Close db connections
+function disconnectAll() {
+    pg.end();
+}
