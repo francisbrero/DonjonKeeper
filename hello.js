44aef31a2ce9dd8649b738590b16ee64156c78c6
Francis-a1 <francis.brero@agilone.com>
2014-02-16 16:43:28 -0800

    postgre2

--- hello.js
+++ hello.js
@@ -18,20 +18,20 @@ app.listen(port, function() {
 app.get('/', function(req, res) {
   client = new pg.Client(connectionString);
   client.connect();
-  query = client.query('SELECT status from donjonkeeper order by dateModified DESC limit 1');
+  query = client.query('SELECT status AS currentStatus from donjonkeeper order by dateModified DESC limit 1');
   query.on('row', function(result) {
     console.log(result);
 
     if (!result) {
       return res.send('No data found');
     } else {
-      res.send('Current Status: ' + result.rows[0].status);
+      res.send('Current Status: ' + result.rows[0].currentStatus);
     }
   });
 });
 
 //returns current status
-function changeStatus(onDone){
+/*function changeStatus(onDone){
  pg.connect(connectionString, function(err, client, done) {
   //client.query('SELECT status from donjonkeeper order by dateModified DESC limit 1', function(err, result) {
   client.query('insert into donjonkeeper (status, dateModified) values (0, now())', function(err, result) {
@@ -42,7 +42,7 @@ function changeStatus(onDone){
    });
   });
 }
-
+*/
 // Close db connections
 function disconnectAll() {
     pg.end();
