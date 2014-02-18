f75f4240993d4102e454435d23a7ce0d8797017a
Francis-a1 <francis.brero@agilone.com>
2014-02-17 19:24:38 -0800

    python - working
    bridge turns the led on/off

--- python/script.py
+++ python/script.py
@@ -1,9 +1,9 @@
 from urllib2 import Request, urlopen, URLError 
 import json
 import time ## Import 'time' library. Allows us to use 'sleep'
-#import RPi.GPIO as GPIO ## Import GPIO library
-#GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-#GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+import RPi.GPIO as GPIO ## Import GPIO library
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 
 # Let's create a Bridge, yiha!!
 while True:
@@ -16,11 +16,11 @@ while True:
 		currentstatus = j[0]
 		status = currentstatus['status']	
 		if status == 'open':
-			#GPIO.output(7,true) ## Turn on GPIO pin 7
-			print 'yiha'
+			GPIO.output(7,true) ## Turn on GPIO pin 7
+			#print 'yiha'
 		else:
-			#GPIO.output(7,false) ## Turn on GPIO pin 7
-			print 'bouhou'
+			GPIO.output(7,false) ## Turn on GPIO pin 7
+			#print 'bouhou'
 	except URLError, e:
 		print 'No kittez. Got an error code:', e
 	time.sleep(1)
