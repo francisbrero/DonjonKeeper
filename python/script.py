15c3349153780cdddd3b491df46980ba6e68c9dd
Francis-a1 <francis.brero@agilone.com>
2014-02-17 19:17:32 -0800

    python full

---
+++ python/script.py
@@ -0,0 +1,26 @@
+from urllib2 import Request, urlopen, URLError 
+import json
+import time ## Import 'time' library. Allows us to use 'sleep'
+#import RPi.GPIO as GPIO ## Import GPIO library
+#GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+#GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+
+# Let's create a Bridge, yiha!!
+while True:
+	#Make the API call
+	request = Request('http://donjonkeeper.herokuapp.com/GET/currentstatus')
+
+	try:
+		response = urlopen(request)
+		j = json.loads(response.read())
+		currentstatus = j[0]
+		status = currentstatus['status']	
+		if status == 'open':
+			#GPIO.output(7,true) ## Turn on GPIO pin 7
+			print 'yiha'
+		else:
+			#GPIO.output(7,false) ## Turn on GPIO pin 7
+			print 'bouhou'
+	except URLError, e:
+		print 'No kittez. Got an error code:', e
+	time.sleep(1)
